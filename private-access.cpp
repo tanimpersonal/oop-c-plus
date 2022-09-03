cf76da559beb04ac6153598280e93c0f61ed7a70
tanimpersonal <tashintanim2019@gmail.com>
2022-09-04 03:08:29 +0600

    private-access

---
+++ private-access.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Roll{
+private:
+    int roll;
+public:
+    void setter(int a){
+        roll=a;
+    }
+    //getter
+    int getter(){
+        return roll;
+    }
+};
+int main(){
+    Roll name;
+    //call the setter to assign value to private roll
+    name.setter(20);
+    cout << name.getter();
+}
